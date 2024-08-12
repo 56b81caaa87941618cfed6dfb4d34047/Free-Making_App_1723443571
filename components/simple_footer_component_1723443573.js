e510f0e8c9924ccb70112ef41132a1d9ada76081
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-12 06:19:50 +0000

    Placeholder text replacement

--- components/simple_footer_component_1723443573.js
+++ components/simple_footer_component_1723443573.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1723443573", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            FreedomApp
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Join the movement for a freer world. Download FreedomApp and take control of your digital life.
                     </div>
                 </div>
             </div>
